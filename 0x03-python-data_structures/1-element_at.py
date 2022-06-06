cebf29e8be46b7e5a0d3a8b312ce885d3af5af55
sandra andeka <85032976+sandraandeka@users.noreply.github.com>
2022-06-06 16:15:34 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,11 +2,4 @@
 
 
 def element_at(my_list, idx):
-    """
-    Gets an element in a list at a given index 
-    And return it
-    """
-    if idx >= len(my_list) or idx < 0:
-        return
-
-    return (my_list[idx])
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
